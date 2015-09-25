97142bda92166296f3584ae13323d54314058bf0
André Gustavo Rigon <andregr@touchtec.com.br>
2015-09-25 13:13:50 -0300

    Simpler haskell-mode installation and configuration
    Change-Id: I14bb134fdf5a4250a7c7b48c461e19808c8c3034

--- init.el
+++ init.el
@@ -16,30 +16,6 @@
     (when (memq window-system '(mac ns))
       (exec-path-from-shell-initialize))))
 
-(defun init-customize-el-get ()
-  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-  (unless (require 'el-get nil 'noerror)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  
-  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user")
-  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/haskell")
-  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/coq")
-
-  (require 'el-get-elpa)
-  
-  (setq
-   my:el-get-packages
-   '(el-get                  ; el-get is self-hosting
-     solarized-emacs
-     esup
-     haskell-mode
-     ProofGeneral4.3))
-  (el-get 'sync my:el-get-packages))
-
 (defun init-customize-display ()
   (init-message "Customizing display")
   
@@ -64,6 +40,7 @@
   
   (cua-mode)
   (global-set-key (kbd "C-,") 'switch-to-buffer)
+  (global-set-key (kbd "C-.") 'delete-other-windows)  
 
   (defun comment ()
     (interactive)
@@ -82,6 +59,31 @@
   (global-set-key (kbd "M-;") 'comment)
   
   (global-set-key (kbd "C-x C-b") 'ibuffer))
+
+(defun init-customize-el-get ()
+  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+  (unless (require 'el-get nil 'noerror)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  
+  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user")
+  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/haskell")
+  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/coq")
+
+  (require 'el-get-elpa)
+  
+  (setq
+   my:el-get-packages
+   '(el-get                  ; el-get is self-hosting
+     solarized-emacs
+     esup
+     haskell-mode
+     ProofGeneral4.3))
+  (el-get 'sync my:el-get-packages))
+
 
 (defun init-customize-visual ()
   (ido-mode)
@@ -105,17 +107,17 @@
   (setq vc-handled-backends nil))
 
 (defun init-customize-haskell ()
-  ;; Haskell mode is defined by the recipes in ~/.emacs.d/el-get-user/haskell,
-  ;; which are cloned from https://github.com/dysinger/el-get/tree/haskell/recipes.
+  ;; Haskell mode is defined by the recipe in ~/.emacs.d/el-get-user/haskell.
 
   ;; Setup interactive mode
+  (setq haskell-process-path-cabal "~/.cabal/bin/cabal")
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
 
 (init-message "Start")
 (init-customize-basic-settings)
-(init-customize-el-get)
 (init-customize-display)
 (init-customize-bindings)
+(init-customize-el-get)
 (init-customize-visual)
 (init-customize-git)
 (init-customize-haskell)
